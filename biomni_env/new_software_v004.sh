ac7c9991af4db5dc48b65c3a70a347b5edbbd5c7
Zhuoyan Li <64866606+zskylarli@users.noreply.github.com>
2025-07-14 16:20:21 -0400

    add panhumanpy installation

---
+++ biomni_env/new_software_v004.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Assuming biomni_e1 environment is activated
+
+# Installing panhumanpy
+echo -e "Installing panhumanpy..."
+pip install git+https://github.com/satijalab/panhumanpy.git
+echo -e "panhumanpy installation via Git is complete." 
