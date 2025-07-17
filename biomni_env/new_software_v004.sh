d7f041b16e6bb024f2b3b8c6979d1fb4d1ec8ca3
serena2z <serena2zhang@gmail.com>
2025-07-17 15:20:28 -0700

    created separate env for panhumanpy due to dependency conflicts with biomni_e1

--- biomni_env/new_software_v004.sh
+++ biomni_env/new_software_v004.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
 # Assuming biomni_e1 environment is activated
-
-# Installing panhumanpy
-echo -e "Installing panhumanpy..."
-pip install git+https://github.com/satijalab/panhumanpy.git
-echo -e "panhumanpy installation via Git is complete."
